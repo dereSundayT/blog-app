aea8f3f5bb6ce65001bd9f8b6ad4e5708ead6803
dereSundayT <toluwanisundaydere@gmail.com>
2023-06-17 09:42:23 +0100

    classone

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,23 @@
+
+
+
+const Blog =  () => {
+    return (
+        <>
+        {/*  Parent Wrapper  */}
+            <div>
+            {/* Banner Section   */}
+                <div>
+
+                </div>
+
+            {/*    Main Body Section*/}
+                <div>
+
+                </div>
+
+            </div>
+
+        </>
+    )
+}
